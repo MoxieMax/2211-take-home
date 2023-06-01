9729a50499cf3a47f9e1c9d24cfc8b93fd91f523
Max MacGillivray <mary.macgillivray@gmail.com>
2023-06-01 00:28:03 -0700

    Updated the migrations and schema to correct an error with foreign keys

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :teas, through: :subscriptions
+  
+  validates :first_name, :last_name, :email, :address, presence: true
 end
--- db/migrate/20230601070021_create_subscriptions.rb
+++ db/migrate/20230601070021_create_subscriptions.rb
@@ -5,8 +5,8 @@ class CreateSubscriptions < ActiveRecord::Migration[7.0]
       t.float :price
       t.integer :status
       t.integer :frequency
-      t.references :customer_id, null: false, foreign_key: true
-      t.references :tea_id, null: false, foreign_key: true
+      t.references :customer, null: false, foreign_key: true
+      t.references :tea, null: false, foreign_key: true
 
       t.timestamps
     end
