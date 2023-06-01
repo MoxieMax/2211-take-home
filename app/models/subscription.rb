05083b67904a208d7979d9a0bca4c7d70d6ae440
Max MacGillivray <mary.macgillivray@gmail.com>
2023-06-01 01:18:55 -0700

    Added test for relationships and validations for Subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,13 @@
 class Subscription < ApplicationRecord
-  belongs_to :customer_id
-  belongs_to :tea_id
+  belongs_to :customer
+  belongs_to :tea
+  
+  validates_presence_of :title, 
+                        :price, 
+                        :status, 
+                        :frequency
+  
+  # validates :title, :price, :status, :frequency, presence: true
+  
+  enum status: { 'active' => 0, 'cancelled' => 1 }
 end
