29c5e8df8c65a97335fc32e25a9ac0c796c56c42
Max MacGillivray <mary.macgillivray@gmail.com>
2023-06-01 01:12:18 -0700

    Updated factories to include Faker data

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :customer do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+    first_name { Faker::Games::DnD.first_name }
+    last_name { Faker::Games::DnD.last_name }
     email { Faker::Internet.email }
     address { Faker::Address.full_address }
   end
--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :subscription do
-    title { "MyString" }
-    price { 1.5 }
-    status { 1 }
-    frequency { 1 }
-    customer_id { nil }
-    tea_id { nil }
+    title { Faker::Fantasy::Dune.title }
+    price { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
+    status { Faker::Number.within(range: 0..1) }
+    frequency { Faker::Number.within(range: 1..12) }
   end
 end
